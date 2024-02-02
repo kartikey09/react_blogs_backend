31b3e02f6a35efa017be4878ae322e27c5ace5a6
karikey rana <rana.kartikey999@gmail.com>
2024-02-02 23:16:57 +0530

    fix4

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const followRouter = require('./routers/followRouter');
 const unfollowRouter = require('./routers/unFollowRoute');
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
+app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type','Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
